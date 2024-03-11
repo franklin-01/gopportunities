d0260dcaca5bdeabd00bd986599c691e00bf7f15
Franklin <franklinmatos8@gmail.com>
2024-03-11 00:22:05 -0300

    feat: add createOpening handler logic

---
+++ handler/handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"github.com/franklin-01/gopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
